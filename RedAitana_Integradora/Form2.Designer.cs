38ba9871040658ead47dd05914cbd2332887dff3
GreteldDev <greteldairen@gmail.com>
2025-06-15 23:26:04 -0500

    Primer commit
    Primer commit del proyecto RedAitana Integradora

---
+++ RedAitana_Integradora/Form2.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace RedAitana_Integradora
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(71, 48, 120);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(170, 447);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(117, 76, 154);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 61);
+            button1.TabIndex = 0;
+            button1.Text = "Registro General";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(265, 114);
+            label1.Name = "label1";
+            label1.Size = new Size(465, 106);
+            label1.TabIndex = 1;
+            label1.Text = "Bienvenido";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(410, 244);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(154, 119);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 38);
+            label2.TabIndex = 1;
+            label2.Text = "RedAitana";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(117, 76, 154);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(12, 197);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 61);
+            button2.TabIndex = 2;
+            button2.Text = "Gestión Empleados";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(117, 76, 154);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(13, 280);
+            button3.Name = "button3";
+            button3.Size = new Size(145, 61);
+            button3.TabIndex = 3;
+            button3.Text = "Gestión Voluntarios";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button3;
+        private Button button2;
+        private Label label2;
+    }
+}
---
+++ RedAitana_Integradora/Form4.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace RedAitana_Integradora
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            label2 = new Label();
+            button1 = new Button();
+            label1 = new Label();
+            label3 = new Label();
+            panel2 = new Panel();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button4 = new Button();
+            button5 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(71, 48, 120);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(170, 450);
+            panel1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(117, 76, 154);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(13, 280);
+            button3.Name = "button3";
+            button3.Size = new Size(145, 61);
+            button3.TabIndex = 3;
+            button3.Text = "Gestión Voluntarios";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(117, 76, 154);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(12, 197);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 61);
+            button2.TabIndex = 2;
+            button2.Text = "Gestión Empleados";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(12, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 38);
+            label2.TabIndex = 1;
+            label2.Text = "RedAitana";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(223, 128, 108);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(13, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 61);
+            button1.TabIndex = 0;
+            button1.Text = "Registro General";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(203, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(365, 38);
+            label1.TabIndex = 5;
+            label1.Text = "Registro de Entrada/Salida";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(12, 14);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Visita General";
+            label3.Click += label3_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(177, 135, 193);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(220, 102);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(518, 47);
+            panel2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(278, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Nombre";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(278, 238);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Apellido";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(278, 280);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Fecha";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(278, 321);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 20);
+            label7.TabIndex = 11;
+            label7.Text = "Hora";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(382, 188);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(317, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(382, 235);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(317, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(382, 280);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(317, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(382, 318);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(317, 27);
+            textBox4.TabIndex = 15;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(177, 135, 193);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(376, 376);
+            button4.Name = "button4";
+            button4.Size = new Size(158, 52);
+            button4.TabIndex = 16;
+            button4.Text = "Registrar Entrada";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += this.button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(177, 135, 193);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(557, 376);
+            button5.Name = "button5";
+            button5.Size = new Size(158, 52);
+            button5.TabIndex = 17;
+            button5.Text = "Registrar Salida";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(panel2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Name = "Form4";
+            Text = "Form4";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button3;
+        private Button button2;
+        private Label label2;
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private Panel panel2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button4;
+        private Button button5;
+    }
+}
